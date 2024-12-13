344f06c08a17f1e526383f9498e61b2eafcb47f9
drblacktran <gtra0011@student.monash.edu>
2024-12-13 19:54:49 +1100

    Nav Bar Component before switch to PrimeNG

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 import {NgOptimizedImage} from "@angular/common";
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatRipple} from "@angular/material/core";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
+    MatToolbar,
+    MatRipple
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
 
+  currentLanguage: string = 'VN';
+  toggleLanguage() {
+    this.currentLanguage = this.currentLanguage === 'VN' ? 'US' : 'VN';
+  }
 }
