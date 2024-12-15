47048a3012965eec661fc324deb23092aad8bd2d
drblacktran <gtra0011@student.monash.edu>
2024-12-16 00:39:17 +1100

    Push for Design Choosing in Culture Values #TODO:
    - Add Dash line by Color with Effect
    - Restructure the whole below div
    - Update the Nav Bar Border Bottom to By the Navigator of the Page (Using Linear Gradient)

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import {NgForOf, NgStyle} from "@angular/common";
+import {NgForOf, NgIf, NgStyle} from "@angular/common";
 import {ProgramsComponent} from "../programs/programs.component";
 import {SponsorComponent} from "../sponsor/sponsor.component";
+import {DividerModule} from "primeng/divider";
 
 @Component({
   selector: 'app-dashboard',
@@ -10,12 +11,38 @@ import {SponsorComponent} from "../sponsor/sponsor.component";
     NgStyle,
     NgForOf,
     ProgramsComponent,
-    SponsorComponent
+    SponsorComponent,
+    NgIf,
+    DividerModule
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+
+  cultureLines = [
+    {
+      text: '#1 - Chia sẻ và phát triển',
+      color: '#CF5258', // Red tone
+      image: 'assets/culture1.png', // Replace with your image paths
+    },
+    {
+      text: '#2 - Trân trọng sự khác biệt',
+      color: '#E19E29', // Orange tone
+      image: 'assets/culture2.png',
+    },
+    {
+      text: '#3 - Cam kết với nhiệm vụ',
+      color: '#78A4A3', // Teal tone
+      image: 'assets/culture3.png',
+    },
+    {
+      text: '#4 - Lắng nghe và thấu hiểu',
+      color: '#444B5B', // Dark grey/blue tone
+      image: 'assets/culture4.png',
+    },
+  ];
+
   rowOneData = [
     {
       number: '06',
